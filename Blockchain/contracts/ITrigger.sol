deb586312f0234fa542a370c762c05c4df136a68
theomgdev <trincitasdad@gmail.com>
2024-07-01 16:26:55 +0300

    Bump up Solidity version from 0.8.20 to 0.8.26

--- Blockchain/contracts/ITrigger.sol
+++ Blockchain/contracts/ITrigger.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 // Compatible with OpenZeppelin Contracts ^5.0.0
 
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.26;
 
 interface ITrigger {
     function trigger() external;
